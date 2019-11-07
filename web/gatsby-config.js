d8720479975023dc4d422903972b89fa8877d0f9
Jackie Luc <jackieluc17@gmail.com>
2019-11-06 22:03:17 -0700

    chore: add gatsby-source-sanity, eslint, prettier configs

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,34 +1,74 @@
+require('dotenv').config()
+const {
+  api: { projectId, dataset }
+} = requireConfig('../studio/sanity.json')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: '// Jackie Luc',
+    description: "Jackie Luc's personal website.",
+    author: 'Jackie Luc'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId,
+        dataset,
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
+}
+
+/**
+ * We're requiring a file in the studio folder to make the monorepo
+ * work "out-of-the-box". Sometimes you would to run this web frontend
+ * in isolation (e.g. on codesandbox). This will give you an error message
+ * with directions to enter the info manually or in the environment.
+ */
+
+function requireConfig (path) {
+  try {
+    return require('../studio/sanity.json')
+  } catch (e) {
+    console.error(
+      'Failed to require sanity.json. Fill in projectId and dataset name manually in gatsby-config.js'
+    )
+    return {
+      api: {
+        projectId: process.env.SANITY_PROJECT_ID || '',
+        dataset: process.env.SANITY_DATASET || ''
+      }
+    }
+  }
 }
